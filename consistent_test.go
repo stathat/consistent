09a7bd1e4a9791e9dcd2f2821f1873eafa39b61c
jeff <leterip@gmail.com>
2012-07-12 16:01:49 -0400

    added some benchmarks for common operations and allocations

--- consistent_test.go
+++ consistent_test.go
@@ -1,7 +1,9 @@
 package consistent
 
 import (
+	"runtime"
 	"sort"
+	"strconv"
 	"testing"
 	"testing/quick"
 )
@@ -262,5 +264,92 @@ func TestGetTwoOnlyOneInCircle(t *testing.T) {
 	}
 	if b != "" {
 		t.Errorf("wrong b: %q", b)
+	}
+}
+
+// allocBytes returns the number of bytes allocated by invoking f. 
+func allocBytes(f func()) uint64 {
+	var stats runtime.MemStats
+	runtime.ReadMemStats(&stats)
+	t := stats.TotalAlloc
+	f()
+	runtime.ReadMemStats(&stats)
+	return stats.TotalAlloc - t
+}
+
+func mallocNum(f func()) uint64 {
+	var stats runtime.MemStats
+	runtime.ReadMemStats(&stats)
+	t := stats.Mallocs
+	f()
+	runtime.ReadMemStats(&stats)
+	return stats.Mallocs - t
+}
+
+func BenchmarkAllocations(b *testing.B) {
+	x := New()
+	x.Add("stays")
+	b.ResetTimer()
+	allocSize := allocBytes(func() {
+		for i := 0; i < b.N; i++ {
+			x.Add("Foo")
+			x.Remove("Foo")
+		}
+	})
+	b.Logf("%d: Allocated %d bytes (%.2fx)", b.N, allocSize, float64(allocSize)/float64(b.N))
+}
+
+func BenchmarkMalloc(b *testing.B) {
+	x := New()
+	x.Add("stays")
+	b.ResetTimer()
+	mallocs := mallocNum(func() {
+		for i := 0; i < b.N; i++ {
+			x.Add("Foo")
+			x.Remove("Foo")
+		}
+	})
+	b.Logf("%d: Mallocd %d times (%.2fx)", b.N, mallocs, float64(mallocs)/float64(b.N))
+}
+
+func BenchmarkCycle(b *testing.B) {
+	x := New()
+	x.Add("nothing")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x.Add("foo" + strconv.Itoa(i))
+		x.Remove("foo" + strconv.Itoa(i))
+	}
+}
+
+func BenchmarkCycleLarge(b *testing.B) {
+	x := New()
+	for i := 0; i < 10; i++ {
+		x.Add("start" + strconv.Itoa(i))
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x.Add("foo" + strconv.Itoa(i))
+		x.Remove("foo" + strconv.Itoa(i))
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	x := New()
+	x.Add("nothing")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x.Get("nothing")
+	}
+}
+
+func BenchmarkGetLarge(b *testing.B) {
+	x := New()
+	for i := 0; i < 10; i++ {
+		x.Add("start" + strconv.Itoa(i))
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x.Get("nothing")
 	}
 }
