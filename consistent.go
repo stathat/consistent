a08777fd1640b18c031319397a4c60832f5f396f
jeff <leterip@gmail.com>
2012-07-17 18:26:53 -0400

    Only reallocate our sortedHashes value if it's way too big
    Benchmark comparisons:
    
    benchmark               old ns/op    new ns/op    delta
    BenchmarkAllocations        36460        33516   -8.07%
    BenchmarkMalloc             35484        33182   -6.49%
    BenchmarkCycle              35192        32601   -7.36%
    BenchmarkCycleLarge        143203       132879   -7.21%
    BenchmarkGet                  573          581   +1.40%
    BenchmarkGetLarge             683          689   +0.88%
    
    Allocation/mallocs:
    
    OLD: consistent_test.go:299: 50000: Allocated 72001744 bytes (1440.03x)
    NEW: consistent_test.go:299: 50000: Allocated 33602000 bytes (672.04x)
    
    OLD: consistent_test.go:312: 50000: Mallocd 4750002 times (95.00x)
    NEW: consistent_test.go:312: 50000: Mallocd 4100004 times (82.00x)

--- consistent.go
+++ consistent.go
@@ -142,7 +142,11 @@ func (c *Consistent) hashKey(key string) uint32 {
 }
 
 func (c *Consistent) updateSortedHashes() {
-	hashes := uints(nil)
+	hashes := c.sortedHashes[:0]
+	//reallocate if we're holding on to too much (1/4th)
+	if cap(c.sortedHashes)/(c.NumberOfReplicas*4) > len(c.circle) {
+		hashes = nil
+	}
 	for k := range c.circle {
 		hashes = append(hashes, k)
 	}
